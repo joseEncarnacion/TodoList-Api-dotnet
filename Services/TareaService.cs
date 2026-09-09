f116338a71e5568e49ed3230cbfc6b2e30b5203b
Jose Luis Encarnacion <jose.lencarnacion1@gmail.com>
2026-09-09 16:06:52 -0400

    documentacion: Implementamos estructura de logging en tarea services

--- Services/TareaService.cs
+++ Services/TareaService.cs
@@ -15,24 +15,32 @@ namespace todo.api.Services
     {
         private readonly AppDbContext _context;
         private readonly TareaValidator _validator;
+        private readonly ILogger<TareaService> _logger;
 
-        public TareaService(AppDbContext context, TareaValidator validator)
+        public TareaService(AppDbContext context, TareaValidator validator, ILogger<TareaService> logger)
         {
             _context = context;
             _validator = validator;
+            _logger = logger;
         }
         public async Task<TareaResponseDTOs?> Actualizar(int id, TareaUpdateDTOs dto)
         {
            var actualizarTarea  = await _context.Tareas
            .FirstOrDefaultAsync(t => t.TareaID == id);
 
-           if (actualizarTarea == null) return null;
+           if (actualizarTarea == null)
+            {
+                _logger.LogWarning("No se encontró la tarea {TareaID} para actualizar",id);
+                return null;
+            }
 
            await _validator.ValidarActualizar(dto);
 
            TareaMapper.UpdateEntity(actualizarTarea, dto); 
 
            await _context.SaveChangesAsync();
+
+            _logger.LogInformation("La tarea {TareaId} fue actualizada correctamente",id);
 
            
             var tareaActualizada = await _context.Tareas
@@ -54,6 +62,8 @@ namespace todo.api.Services
             _context.Tareas.Add(tarea);
             await _context.SaveChangesAsync();
 
+           _logger.LogInformation("La tarea {TareaId} fue creada correctamente",tarea.TareaID);
+
             var tareaGuardada = await _context.Tareas
             .Where(t => t.TareaID == tarea.TareaID)
             .Select(TareaMapper.ToResponse())
@@ -67,10 +77,18 @@ namespace todo.api.Services
             var tarea =  await _context.Tareas
             .FirstOrDefaultAsync(t => t.TareaID == id);
 
-            if(tarea == null) return false;
+            if(tarea == null) {
+
+                _logger.LogWarning("No se encontró la tarea {TareaID} para eliminar",id);
+                return false;
+
+            }
+
 
             _context.Tareas.Remove(tarea);
             await _context.SaveChangesAsync();
+
+            _logger.LogInformation("La tarea {TareaId} fue eliminada correctamente",id);
 
             return true;
         }
@@ -93,21 +111,42 @@ namespace todo.api.Services
             .Select(TareaMapper.ToResponse())
             .ToListAsync();
 
+
+
             return response;
         }
 
         public async Task<List<TareaResponseDTOs>> Listar()
         {
-            return await _context.Tareas
+
+            var tareas = await _context.Tareas
             .Select(TareaMapper.ToResponse()).ToListAsync();
+
+            _logger.LogInformation("Se obtuvieron {CantidadTareas} tareas", tareas.Count);
+            return tareas;
+
         }
 
         public async Task<TareaResponseDTOs?> ObtenerPorID(int id)
         {
-            return await _context.Tareas
+        
+            var tarea =  await _context.Tareas
             .Where(t => t.TareaID == id)
             .Select(TareaMapper.ToResponse())
             .FirstOrDefaultAsync();
+
+            if (tarea == null)
+            {
+             _logger.LogWarning("No se encontró la tarea {TareaId} solicitada",id);
+
+             return null;
+                
+            }
+
+             _logger.LogInformation("Se obtuvo correctamente la tarea {TareaId}",id);
+
+            return tarea;
+
 
         }
     }
